329e06274b4b0efcdfc1fd9e93b8e0fad1210a00
Yucky <registyucky@gmail.com>
2021-06-23 20:41:59 +0900

    add function to select target_dir

--- copy_file.py
+++ copy_file.py
@@ -9,8 +9,15 @@ root.withdraw()
 source = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("XLSX Files","*.xlsx"), ("all files", "*.*")))
 
 target = 'import'
-dir_parts = list(os.path.split(source))
-target_dir = dir_parts[0] + '/' + target + '/' + dir_parts[1]
+# dir_parts = list(os.path.split(source))
+# target_dir = dir_parts[0] + '/' + target + '/' + dir_parts[1]
 # print(target_dir)
 
-copy2(source, target_dir)
+from tkinter import filedialog
+from tkinter import *
+root = Tk()
+root.withdraw()
+target_dir = filedialog.askdirectory()
+print(target_dir)
+
+# copy2(source, target_dir)
