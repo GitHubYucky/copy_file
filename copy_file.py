84f011110126ccc9f3d435a86f3890e9ba730617
Yucky <registyucky@gmail.com>
2021-06-23 20:46:22 +0900

    works fine

--- copy_file.py
+++ copy_file.py
@@ -7,17 +7,20 @@ root = Tk()
 root.withdraw()
 
 source = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("XLSX Files","*.xlsx"), ("all files", "*.*")))
+source = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("XLSX Files","*.xlsx"), ("all files", "*.*")))
 
-target = 'import'
-# dir_parts = list(os.path.split(source))
+# target = 'import'
+dir_parts = list(os.path.split(source))
+file_name=dir_parts[1]
+# print(file_name)
 # target_dir = dir_parts[0] + '/' + target + '/' + dir_parts[1]
 # print(target_dir)
 
-from tkinter import filedialog
-from tkinter import *
+
 root = Tk()
 root.withdraw()
 target_dir = filedialog.askdirectory()
-print(target_dir)
+target_dir=target_dir + '/'
+# print(target_dir)
 
-# copy2(source, target_dir)
+copy2(source, target_dir)
