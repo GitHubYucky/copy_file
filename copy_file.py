65aee97f7dc08f43843f3e4de1ed747e9065fd23
Yucky <registyucky@gmail.com>
2021-06-23 20:25:21 +0900

    copied template

---
+++ copy_file.py
@@ -0,0 +1,7 @@
+import shutil
+import tkinter.filedialog
+
+original = r'original path where the file is currently stored\file name.file extension'
+target = r'target path where the file will be copied\file name.file extension'
+
+shutil.copyfile(original, target)
